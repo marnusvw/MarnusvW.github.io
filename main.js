d0486d75c6b713adb83c7d273f189ea3edf964c1
Marnus von Wielligh <52937047+marnusvw@users.noreply.github.com>
2024-07-01 14:49:28 +0200

    Add JS function to open new windows to all the references.

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
-let test = document.getElementById('test');
+let social_share = document.getElementById("gmail_share");
+let git_page = document.getElementById("git_hub");
+function gmailShare(url) {
+  window.open(
+    (url =
+      "https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=marnusvw7@gmail.com&body=my-text"),
+    "_blank",
+    "sharer",
+    "toolbar=0,status=0,width=648,height=395"
+  );
+  return true;
+}
+function githubShare(url) {
+  window.open(
+    (url =
+      "https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=marnusvw7@gmail.com&body=my-text"),
+    "_blank",
+    "sharer",
+    "toolbar=0,status=0,width=648,height=395"
+  );
+  return true;
+}
 
-test.addEventListener('click', () => {
-    alert('Hello World');
-    
-})
+social_share.addEventListener("click", gmailShare);
